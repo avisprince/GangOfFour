6e9ac84f034e74206bcffcb161a32b37147730f7
aprince <aprince@vistaprint.com>
2017-03-08 17:37:46 -0500

    Add singleton pattern

---
+++ Singleton/Singleton/SingleObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class SingleObject
+    {
+        private static SingleObject instance = new SingleObject();
+
+        private SingleObject() { }
+
+        public static SingleObject GetInstance()
+        {
+            return instance;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Running single object class");
+        }
+    }
+}
