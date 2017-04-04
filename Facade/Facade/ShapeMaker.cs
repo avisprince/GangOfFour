6f6352f72c478e431d8b8e699bb0f3e1ca1f9998
aprince <aprince@vistaprint.com>
2017-04-04 12:03:31 -0400

    add facade pattern

---
+++ Facade/Facade/ShapeMaker.cs
@@ -0,0 +1,40 @@
+﻿using Facade.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class ShapeMaker
+    {
+        public ShapeMaker()
+        {
+            this.Square = new Square();
+            this.Circle = new Circle();
+            this.Rectangle = new Rectangle();
+        }
+
+        public IShape Square { get; private set; }
+
+        public IShape Circle { get; private set; }
+
+        public IShape Rectangle { get; private set; }
+
+        public void DrawSquare()
+        {
+            this.Square.Draw();
+        }
+
+        public void DrawCircle()
+        {
+            this.Circle.Draw();
+        }
+
+        public void DrawRectangle()
+        {
+            this.Rectangle.Draw();
+        }
+    }
+}
