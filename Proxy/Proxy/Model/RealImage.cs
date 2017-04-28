1592ed7faf5988fbe5c9d20a785f0b2e04104bba
aprince <aprince@vistaprint.com>
2017-04-28 11:29:57 -0400

    add proxy pattern

---
+++ Proxy/Proxy/Model/RealImage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.Model
+{
+    public class RealImage : IImage
+    {
+        private string fileName;
+
+        public RealImage(string fileName)
+        {
+            this.fileName = fileName;
+            LoadFromDisk(fileName);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Displaying: " + this.fileName);
+        }
+
+        private void LoadFromDisk(string fileName)
+        {
+            Console.WriteLine("Loading: " + fileName);
+        }
+    }
+}
---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,23 @@
+﻿using Proxy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IImage image = new ProxyImage("test_10mb.jpg");
+
+            // image will be loaded from disk
+            image.Display();
+
+            // image will not be loaded from disk
+            image.Display();
+        }
+    }
+}
